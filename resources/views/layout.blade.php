1f7304dc00b0c18d7b9268f820c4c201f315c2f2
Todd Libet <toddlibet@Todds-MacBook-Pro-2.local>
2017-02-02 22:22:43 -0600

    Chat Functionality

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ora Interactive Chat Code Challenge</title>
+  </head>
+  <body>
+    @yield('chatWindow');
+    @yield('sendMessage');
+  </body>
+</html>
